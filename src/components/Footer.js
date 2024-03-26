cf663cea62e4387e6faeb3a86c8e8da3abfdfd28
personal <nirfarakeaaditi@gmail.com>
2024-03-26 20:10:05 +0530

    ADD: twitter handle added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -30,7 +30,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                //href=""
+                href="https://x.com/TechWhimAaditi"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
